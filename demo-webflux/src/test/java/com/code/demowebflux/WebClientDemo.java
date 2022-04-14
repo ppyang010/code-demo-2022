b072848daf10f6dff0645315a7f9da0fd431c00f
caicy <caicy@dxy.cn>
2022-04-14 18:30:59 +0800

    2022年04月14日18:28:41

--- demo-webflux/src/test/java/com/code/demowebflux/WebClientDemo.java
+++ demo-webflux/src/test/java/com/code/demowebflux/WebClientDemo.java
@@ -27,12 +27,12 @@ public class WebClientDemo {
     public void testPostRESTClient() throws InterruptedException {
         People people = People.of(4, "赵六", 1);
         WebClient client = WebClient.create(); // 1 使用WebClient.create方法来创建一个新的WebClient对象
-        Flux<People> createdUser = client.post() // 2 使用post方法来创建一个 POST 请求
+        Flux<String> createdUser = client.post() // 2 使用post方法来创建一个 POST 请求
                 .uri("http://localhost:9000/hello2/post") // 3 指定 baseUrl
                 .accept(MediaType.APPLICATION_JSON) // 4 配置请求 Header：Content-Type: application/json
                 .body(Mono.just(people), People.class) // 5 使用body()方法来设置 POST 请求的内容
                 .retrieve() // 6 异步地获取 response 信息，返回值为WebClient.ResponseSpec，retrive()可以看做是exchange()方法的 “快捷版”（exchange()的返回值为ClientResponse）
-                .bodyToFlux(People.class); // 7 WebClient.ResponseSpec的bodyToFlux方法把响应内容转换成User对象，最终得到的结果是Flux<User>
+                .bodyToFlux(String.class); // 7 WebClient.ResponseSpec的bodyToFlux方法把响应内容转换成User对象，最终得到的结果是Flux<User>
 
         createdUser.subscribe((p)-> log.info(JSONUtil.toJsonStr(p))); // 8 打印出来
         TimeUnit.SECONDS.sleep(1); // 9 由于是异步的，我们将测试线程 sleep 1 秒确保拿到 response，也可以用CountDownLatch
