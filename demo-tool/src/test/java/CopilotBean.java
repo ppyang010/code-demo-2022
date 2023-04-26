33547d8fed4ba482e33cc89afc3daaf5d079e116
caicy <caicy@dxy.cn>
2023-04-26 17:18:26 +0800

    copilot 测试

---
+++ demo-tool/src/test/java/CopilotBean.java
@@ -0,0 +1,14 @@
+/**
+ * 根据json生成bean
+ * {
+ *     "code": "success",
+ *     "message": "desc",
+ *     "data": {
+ *         "existUnpaid": true
+ *     }
+ * }
+ *
+ */
+public class CopilotBean {
+
+}
