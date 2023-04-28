9a62b13ab20f5af327bc1ae6e02683cf00304789
caicy <caicy@dxy.cn>
2023-04-28 18:30:14 +0800

    2023年04月28日18:30:11

--- demo-java-17/src/main/java/com/code/comletableFuture/util/UntilTest.java
+++ demo-java-17/src/main/java/com/code/comletableFuture/util/UntilTest.java
@@ -7,6 +7,11 @@ import java.util.concurrent.TimeUnit;
 
 public class UntilTest {
     public static void main(String[] args) throws InterruptedException {
+        utilTimeOutTest();
+        utilCompleteOnTimeOutTest();
+    }
+
+    private static void utilCompleteOnTimeOutTest() throws InterruptedException {
         int a = 10;
         int b = 15;
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
@@ -19,7 +24,30 @@ public class UntilTest {
             }
             return a + b;
         });
-        CompletableFutureTimeout.orTimeout(future,4,TimeUnit.SECONDS)
+        CompletableFutureTimeout.completeOnTimeout(-1, future, 4, TimeUnit.SECONDS)
+                .thenAccept((result) -> {
+                    System.out.println(result);
+                });
+
+        System.out.println("模拟执行其他业务");
+        TimeUnit.SECONDS.sleep(10);
+
+    }
+
+    private static void utilTimeOutTest() throws InterruptedException {
+        int a = 10;
+        int b = 15;
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                int time = RandomUtil.randomInt(3, 8);
+                System.out.println("time=" + time);
+                TimeUnit.SECONDS.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return a + b;
+        });
+        CompletableFutureTimeout.orTimeout(future, 4, TimeUnit.SECONDS)
                 .whenComplete((result, exception) -> {
                     System.out.println(result);
                     if (exception != null)
