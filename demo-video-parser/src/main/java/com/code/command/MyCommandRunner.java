6305b5d690b447c9d20973ba54f55493a59765f3
ppyang010 <644724460@qq.com>
2023-09-16 20:15:12 +0800

    video 2023年9月16日 20:15:03

---
+++ demo-video-parser/src/main/java/com/code/command/MyCommandRunner.java
@@ -0,0 +1,29 @@
+package com.code.command;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyCommandRunner implements CommandLineRunner {
+
+    @Value("${spring.web.loginurl}")
+    private String loginUrl;
+
+    @Value("${spring.auto.openurl}")
+    private boolean isOpen;
+
+    @Override
+    public void run(String... args) {
+        if (isOpen) {
+            System.out.println("自动加载指定的页面");
+            try {
+                Runtime.getRuntime().exec("cmd /c start " + loginUrl);  
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("浏览器打开页面异常");
+            }
+        }
+    }
+
+}
