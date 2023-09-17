3946b9e55b63d1f2e8f93cee5d7584d2761724e0
ppyang010 <644724460@qq.com>
2023-09-17 18:13:09 +0800

    video 2023年9月17日 18:13:02

--- demo-video-parser/src/main/java/com/code/command/BrowserCommandRunner.java
+++ demo-video-parser/src/main/java/com/code/command/BrowserCommandRunner.java
@@ -1,10 +1,12 @@
 package com.code.command;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class BrowserCommandRunner implements CommandLineRunner {
 
     @Value("${spring.web.loginurl}")
@@ -15,15 +17,39 @@ public class BrowserCommandRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        String os = getOs();
         if (isOpen) {
-            System.out.println("自动加载指定的页面");
+            log.info("自动加载指定的页面");
             try {
-                Runtime.getRuntime().exec("cmd /c start " + loginUrl);  
+                Runtime.getRuntime().exec("cmd /c start " + loginUrl);
             } catch (Exception ex) {
                 ex.printStackTrace();
-                System.out.println("浏览器打开页面异常");
+                log.warn("浏览器打开页面异常");
             }
         }
+    }
+
+    public String getOs() {
+        String osName = System.getProperty("os.name");
+        String osVersion = System.getProperty("os.version");
+        String osArch = System.getProperty("os.arch");
+
+        log.info("Operating System Name: " + osName);
+        log.info("Operating System Version: " + osVersion);
+        log.info("Operating System Architecture: " + osArch);
+
+        String os = null;
+        // 判断操作系统类型
+        if (osName.startsWith("Windows")) {
+            os = "Windows";
+        } else if (osName.startsWith("Linux")) {
+            os = "Linux";
+        } else if (osName.startsWith("Mac")) {
+            os = "Mac";
+        } else {
+            os = "unknown";
+        }
+        return os;
     }
 
 }
--- demo-video-parser/src/main/java/com/code/controller/IndexController.java
+++ demo-video-parser/src/main/java/com/code/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping({"/index", "/"})
