90764a392779bba675394fbbce9ba044ebb02b01
caicy <caicy@dxy.cn>
2024-05-11 15:15:14 +0800

    2024年05月11日15:15:09

--- demo-tool/src/main/java/com/code/tool/Tool.java
+++ demo-tool/src/main/java/com/code/tool/Tool.java
@@ -1,15 +1,30 @@
 package com.code.tool;
 
+import cn.hutool.core.util.StrUtil;
+
 /**
  * @author ccy
  * @description
  * @time 2022/6/14 10:19 AM
  */
 public class Tool {
-    public static void main(String[] args) {
-        Integer a = 1 + 1;
-        Integer b = 2;
-        System.out.println(a == b);
-    }
+//    public static void main(String[] args) {
+//        Integer a = 1 + 1;
+//        Integer b = 2;
+//        System.out.println(a == b);
+//    }
+
+
+//    public static void main(String[] args) {
+//        String sqlTmp = "insert into exam_past_user_directory_{}(id,exam_type, class_id, sub_class_id, rel_plan_id, catalogue_no, username)\n" +
+//                "select id+508,exam_type,class_id,sub_class_id,rel_plan_id,'2417101021102',username\n" +
+//                "from exam_past_user_directory_{} where catalogue_no = '2417101021108' and class_id = 17 and created_time<'2024-04-26 18:01:07';";
+//
+//        for (int i = 0; i < 32; i++) {
+//            String format = StrUtil.format(sqlTmp, i, i);
+//            System.out.println(format);
+//        }
+//
+//    }
 
 }
