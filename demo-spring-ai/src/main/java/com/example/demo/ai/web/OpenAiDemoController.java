fa5da358f305bce6dafd94caeed9ef70d7330480
ppyang010 <644724460@qq.com>
2024-08-13 00:36:32 +0800

    2024年8月12日 21:37:19 AI

--- demo-spring-ai/src/main/java/com/example/demo/ai/web/OpenAiDemoController.java
+++ demo-spring-ai/src/main/java/com/example/demo/ai/web/OpenAiDemoController.java
@@ -1,4 +1,24 @@
 package com.example.demo.ai.web;
 
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class OpenAiDemoController {
+
+
+    @Autowired
+    private OpenAiChatClient chatClient;
+//
+//
+//
+//    @GetMapping("/ai/generate")
+//    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+//        return Map.of("generation", chatClient.call(message));
+//    }
 }
